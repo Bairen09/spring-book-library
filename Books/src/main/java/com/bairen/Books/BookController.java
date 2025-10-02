99c3e0c9530d7b8b54dc1edceea8aea96423e3b5
bairan <byerain009@gmail.com>
2025-10-02 23:33:06 +0530

    Trying if there is any problem.

--- Books/src/main/java/com/bairen/Books/BookController.java
+++ Books/src/main/java/com/bairen/Books/BookController.java
@@ -1,18 +1,38 @@
 package com.bairen.Books;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@CrossOrigin(origins="*")
 @RequestMapping("/api/books")
-@CrossOrigin("*")
 public class BookController {
     @Autowired
-    private BookService bookService;
+    private final BookService bookService;
 
     public BookController(BookService bookService) {
         this.bookService = bookService;
+    }
+    @GetMapping
+    public List<Book>getBookList(){
+        return bookService.getBookList();
+    }
+    @GetMapping("/{id}")
+    public Book getBookById(@PathVariable Long id){
+        return bookService.getBookById(id);
+    }
+    @PostMapping
+    public Book addBooks(@RequestBody Book book){
+        return bookService.addBooks(book);
+    }
+    @PutMapping("/{id}")
+    public Book changeBook(@PathVariable Long id, @RequestBody Book bookUpdated){
+        return bookService.changeBook(id,bookUpdated);
+    }
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable Long id){
+        bookService.deleteById(id);
     }
 }
--- Books/src/main/java/com/bairen/Books/BookService.java
+++ Books/src/main/java/com/bairen/Books/BookService.java
@@ -3,12 +3,42 @@ package com.bairen.Books;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BookService {
     @Autowired
-    private BookRepo bookRepo;
+    private final BookRepo bookRepo;
 
     public BookService(BookRepo bookRepo) {
         this.bookRepo = bookRepo;
+    }
+
+    public List<Book> getBookList() {
+        return bookRepo.findAll();
+    }
+
+    public Book getBookById(Long id) {
+        return bookRepo.findById(id).orElseThrow(()->new RuntimeException("Invalid ID"));
+    }
+
+    public Book addBooks(Book book) {
+        if(book.getTitle().isEmpty()|| book.getTitle()==null){
+            throw new IllegalArgumentException("Invalid Exception");
+        }
+        book.setId(null);
+        return bookRepo.save(book);
+    }
+
+    public Book changeBook(Long id, Book bookUpdated) {
+        Book existing = getBookById(id);
+        existing.setTitle(bookUpdated.getTitle());
+        existing.setAuthor(bookUpdated.getAuthor());
+        existing.setYear(bookUpdated.getYear());
+        return bookRepo.save(existing);
+    }
+
+    public void deleteById(Long id) {
+        bookRepo.deleteById(id);
     }
 }
